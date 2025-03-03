e2c27f9589986fc58c1d77394081f7c4bef50f6e
agasurfer <agasurfer@hotmail.com>
2025-03-03 15:02:39 +0100

    footprint model improve

--- models/Footprint.js
+++ models/Footprint.js
@@ -6,12 +6,17 @@ const footprintSchema = new mongoose.Schema({
     total_energy_consumed_kWh: Number,
     carbon_intensity_gCO2e_per_kWh: Number,
     total_carbon_emitted_gCO2e: Number,
+    total_energy_saved_compared_to_standard_kWh: Number,
+    total_carbon_saved_compared_to_standard_gCO2e: Number,
+    footprint_saved_percentage: Number,
       services: [
         {
           serviceId: { type: mongoose.Schema.Types.ObjectId, ref: "Container Volume Bucket" },
           name: String,
           energy_consumed_kWh: Number,
           carbon_emitted_gCO2e: Number,
+          energy_saved_kWh: Number,
+          carbon_saved_gCO2e: Number,
     },
   ],
 });
